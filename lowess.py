cef0aadd9e0071a797d5086d279e48d47045400a
arokem <arokem@gmail.com>
2012-08-26 13:56:14 -0700

    NF: Implemented LOWESS in 1-D.
    Starting to understand the problem, this is a stepping stone to the 3-D case.

--- lowess.py
+++ lowess.py
@@ -13,11 +13,104 @@ Scatterplots. J American Statistical Association, 74: 829-836.
 Loosely based on Matlab(c) code by Kendrick Kay 
 
 """
+import numpy as np
+import scipy.linalg as la
 
+# Kernel functions:
+def epanechnikov(xx, idx):
+    ans = np.zeros(xx.shape)
+    ans[idx] = 0.75 * (1-xx[idx]**2)
+    return ans
+
+def tri_cube(xx, idx):
+    ans = np.zeros(xx.shape)
+    ans[idx] = (1-np.abs(xx[idx])**3)**3
+    return ans
+
+def do_kernel(x0, x, l=1.0, kernel=epanechnikov):
+    """
+    Calculate a kernel function on x in the neighborhood of x0
+
+    Parameters
+    ----------
+    x: float array
+       All values of x
+    x0: float
+       The value of x around which we evaluate the kernel
+    l: float or float array (with shape = x.shape)
+       Width parameter (metric window size)
+    
+    """
+    xx = np.abs(x-x0)
+    idx = np.where(np.abs(xx<=1))
+    return kernel(xx,idx)/l
+
+
+def lowess(x, w, x0, kernel=epanechnikov, l=1):
+    """
+    Locally linear regression with the LOWESS algorithm in 1d
+
+    Parameters
+    ----------
+    x: float array
+       Values of x for which f(x) is known (e.g. measured)
+    
+    w: float array
+       The known values of f(x) at these points 
+
+    x0: float or float array.
+        Values of x for which we estimate the value of f(x) 
+
+    kernel: callable
+        A kernel function. {'epanechnikov', 'tri_cube'}
+
+    l: float or float array with shape = x.shape
+       The metric window size for 
+    Returns
+    -------
+    The function estimated 
+
+    Notes
+    -----
+
+    The solution to this problem is given by equation 6.8 in Friedman, Hastie
+    and Tibshirani (2008). The Elements of Statistical Learning; Chapter 6 
+
+    Example
+    -------
+    >>> import lowess as lo
+    >>> import numpy as np
+    >>> x = np.random.randn(100)
+    >>> f = np.sin(x)
+    >>> x0 = np.linspace(-1,1,10)
+    >>> f_hat = lo.lowess(x, f, x0)
+    >>> import matplotlib.pyplot as plt
+    >>> fig,ax = plt.subplots(1)
+    >>> ax.scatter(x,f)
+    >>> ax.plot(x0,f_hat,'ro')
+    >>> plt.show()
+
+    """
+    # For the case where x0 is provided as a scalar: 
+    if not np.iterable(x0):
+       x0 = np.asarray([x0])
+    ans = np.empty(x0.shape) 
+    # We only need one design matrix:
+    B = np.vstack([np.ones(x.shape[0]), x]).T
+    for idx, this_x0 in enumerate(x0):
+        # Different weighting kernel for each x0:
+        W = np.diag(do_kernel(this_x0, x, l=l, kernel=kernel))
+        # Equation 6.8 in FHT:
+        BtWB = np.dot(np.dot(B.T, W), B)
+        BtW = np.dot(B.T, W)
+        alpha, beta = np.dot(np.dot(la.inv(BtWB), BtW), w)
+        # Estimate the answer based on the parameters:
+        ans[idx] =  alpha + beta * this_x0 
+    return ans
 
 def lowess3d(x,y,z, w, x0, y0, z0, degree, kernel):
     """
-    Locally linear regression with the LOWESS algorithm.
+    Locally linear regression with the LOWESS algorithm in 3d
 
     Parameters
     ----------
@@ -40,7 +133,10 @@ def lowess3d(x,y,z, w, x0, y0, z0, degree, kernel):
     
 
     """
-
+    pass
     
 
+
+
+    
 
--- tests/test_lowess.py
+++ tests/test_lowess.py
@@ -1,25 +1,41 @@
 import numpy as np
 import numpy.testing as npt
-import ..lowess as lo
+import lowess as lo
 
-def test_lowess3d():
-    """ 
 
-    Test local linear regression in 3d
+def test_lowess():
+    """
+    Test 1-d local linear regression with lowess
 
     """
-    # The space is sampled uniformly from -1 to 1:
-    xyz = np.mgrid[-1:1:.1, -1:1:.1, -1:1:.1].reshape(3,20**3)
-    x,y,z = xyz[0],xyz[1],xyz[2]
-    # The w = f(x,y,z)
-    w = sin(x) + cos(y) + tan(z)
-    # Random sample of x,y,z combinations (between -1 and 1):
-    x0 = np.random.randn(1000)
-    y0 = np.random.randn(1000)
-    z0 = np.random.randn(1000)
-    # lowess3d is used to find the values at these sampling points:
-    w0 = lo.lowess3d (x,y,z,w,x0.ravel(), y0.ravel(), z0.ravel())
-    # evaluate f(x,y,z) in the uniformly sampled points:
-    w0actual = sin(x0) + cos(y0) + tan(z0)
-    # How did we do?
-    npt.assert_array_almost_equal(w0, w0actual)
+    x = np.random.randn(100)
+    f = np.sin(x)
+    x0 = np.linspace(-1,1,10)
+    f_hat = lo.lowess(x, f, x0)
+    f_real = np.sin(x0)
+    # This doesn't work, because the algorithm isn't that accurate. Need to
+    # think about something that actually works... 
+    npt.assert_array_almost_equal(f_hat, f_real)
+    
+    
+## def test_lowess3d():
+##     """ 
+
+##     Test local linear regression in 3d
+
+##     """
+##     # The space is sampled uniformly from -1 to 1:
+##     xyz = np.mgrid[-1:1:.1, -1:1:.1, -1:1:.1].reshape(3,20**3)
+##     x,y,z = xyz[0],xyz[1],xyz[2]
+##     # The w = f(x,y,z)
+##     w = sin(x) + cos(y) + tan(z)
+##     # Random sample of x,y,z combinations (between -1 and 1):
+##     x0 = np.random.randn(1000)
+##     y0 = np.random.randn(1000)
+##     z0 = np.random.randn(1000)
+##     # lowess3d is used to find the values at these sampling points:
+##     w0 = lo.lowess3d (x,y,z,w,x0.ravel(), y0.ravel(), z0.ravel())
+##     # evaluate f(x,y,z) in the uniformly sampled points:
+##     w0actual = sin(x0) + cos(y0) + tan(z0)
+##     # How did we do?
+##     npt.assert_array_almost_equal(w0, w0actual)
