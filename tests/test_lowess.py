72b32d31e15a232a418a24d7bab385fb6ddf2076
arokem <arokem@gmail.com>
2012-08-26 12:27:44 -0700

    TST: Changed the logic of the test around.

--- tests/test_lowess.py
+++ tests/test_lowess.py
@@ -8,11 +8,18 @@ def test_lowess3d():
     Test local linear regression in 3d
 
     """
-    x = np.random.rand(10,10,10)
-    y = np.random.rand(10,10,10)
-    z = np.random.rand(10,10,10)
+    # The space is sampled uniformly from -1 to 1:
+    xyz = np.mgrid[-1:1:.1, -1:1:.1, -1:1:.1].reshape(3,20**3)
+    x,y,z = xyz[0],xyz[1],xyz[2]
+    # The w = f(x,y,z)
     w = sin(x) + cos(y) + tan(z)
-    x0,y0,z0 = np.mgrid[-1:1:.1, -1:1:.1, -1:1:.1]
-    w0 = lo.lowess3d (x,y,z,w,x0,y0,z0)
+    # Random sample of x,y,z combinations (between -1 and 1):
+    x0 = np.random.randn(1000)
+    y0 = np.random.randn(1000)
+    z0 = np.random.randn(1000)
+    # lowess3d is used to find the values at these sampling points:
+    w0 = lo.lowess3d (x,y,z,w,x0.ravel(), y0.ravel(), z0.ravel())
+    # evaluate f(x,y,z) in the uniformly sampled points:
     w0actual = sin(x0) + cos(y0) + tan(z0)
-    npt.assert_equal(w0, w0actual)
+    # How did we do?
+    npt.assert_array_almost_equal(w0, w0actual)
