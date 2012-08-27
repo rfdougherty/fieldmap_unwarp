12bf19cc378d3a376fd122a0e8bee6480b311d26
arokem <arokem@gmail.com>
2012-08-27 10:09:05 -0700

    DOC + TST + NF: Lotsa changes (thank providence for long caltrain rides)
    1. Added a single-iteration robustification procedure, along the lines of
    Cleveland 1979's perscription.
    2. Much more documentation.
    3. More extensive testing that actually passes for all test cases (!).

--- lowess.py
+++ lowess.py
@@ -4,23 +4,62 @@ lowess: Locally linear regression
 
 Implementation of the LOWESS algorithm in n dimensions.
 
-Friedman, Hastie and Tibshirani (2008). The Elements of Statistical Learning;
-Chapter 6 
+[FHT] Friedman, Hastie and Tibshirani (2008). The Elements of Statistical
+Learning; Chapter 6 
 
-Cleveland (1979). Robust Locally Weighted Regression and Smoothing
+[Cleveland79] Cleveland (1979). Robust Locally Weighted Regression and Smoothing
 Scatterplots. J American Statistical Association, 74: 829-836.
 
 """
 import numpy as np
 import scipy.linalg as la
+import scipy.stats as stats
 
 # Kernel functions:
-def epanechnikov(xx, idx):
+def epanechnikov(xx, idx=None):
+    """
+    The Epanechnikov kernel estimated for xx values at indices idx (zero
+    elsewhere) 
+
+    Parameters
+    ----------
+    xx: float array
+        Values of the function on which the kernel is computed. Typically,
+        these are Euclidean distances from some point x0 (see do_kernel)
+
+    idx: tuple
+        An indexing tuple pointing to the coordinates in xx for which the
+        kernel value is estimated. Default: None (all points are used!)  
+
+    Notes
+    -----
+    This is equation 6.4 in FHT chapter 6        
+    
+    """        
     ans = np.zeros(xx.shape)
     ans[idx] = 0.75 * (1-xx[idx]**2)
     return ans
 
 def tri_cube(xx, idx):
+    """ 
+    The tri-cube kernel estimated for xx values at indices idx (zero
+    elsewhere) 
+
+    Parameters
+    ----------
+    xx: float array
+        Values of the function on which the kernel is computed. Typically,
+        these are Euclidean distances from some point x0 (see do_kernel)
+
+    idx: tuple
+        An indexing tuple pointing to the coordinates in xx for which the
+        kernel value is estimated. Default: None (all points are used!)  
+
+    Notes
+    -----
+    This is equation 6.6 in FHT chapter 6        
+    """        
+
     ans = np.zeros(xx.shape)
     ans[idx] = (1-np.abs(xx[idx])**3)**3
     return ans
@@ -46,7 +85,25 @@ def do_kernel(x0, x, l=1.0, kernel=epanechnikov):
     return kernel(xx,idx)/l
 
 
-def lowess(x, w, x0, kernel=epanechnikov, l=1):
+def bi_square(xx, idx=None):
+    """
+    The bi-square weight function calculated over values of xx
+
+    Parameters
+    ----------
+    xx: float array
+
+    Notes
+    -----
+    This is the first equation on page 831 of [Cleveland79].
+    """
+
+    ans = np.zeros(xx.shape)
+    ans[idx] = (1-xx[idx]**2)**2
+    return ans
+
+
+def lowess(x, w, x0, kernel=epanechnikov, l=1, robust=False):
     """
     Locally linear regression with the LOWESS algorithm.
 
@@ -70,7 +127,11 @@ def lowess(x, w, x0, kernel=epanechnikov, l=1):
 
     l: float or float array with shape = x.shape
        The metric window size for the kernel
- 
+
+    robust: bool
+        Whether to apply the robustification procedure from [Cleveland79], page
+        831 
+        
     Returns
     -------
     The function estimated at x0. 
@@ -109,6 +170,18 @@ def lowess(x, w, x0, kernel=epanechnikov, l=1):
     >>> ax.scatter(x0[0], x0[1], f_hat, color='r')
 
     """
+
+    if robust:
+        # We use the procedure described in 
+        # Start by calling this function with robust set to false and the x0
+        # input being equal to the x input:
+        w_est = lowess(x, w, x, kernel=epanechnikov, l=1, robust=False)
+        resid = w_est - w
+        median_resid = stats.nanmedian(np.abs(resid))
+        robustness_weights = bi_square(resid/(6*median_resid))
+        # Calculate the bi-cube function on the
+        # Sub-select
+        
     # For the case where x0 is provided as a scalar: 
     if not np.iterable(x0):
        x0 = np.asarray([x0])
@@ -121,9 +194,17 @@ def lowess(x, w, x0, kernel=epanechnikov, l=1):
             this_x0 = np.asarray([this_x0])
         # Different weighting kernel for each x0:
         W = np.diag(do_kernel(this_x0, x, l=l, kernel=kernel))
+
         # XXX It should be possible to calculate W outside the loop, if x0 and
         # x are both sampled in some regular fashion (that is, if W is the same
         # matrix in each iteration). That should save time.
+
+        if robust:
+            # We apply the robustness weights to the weighted least-squares
+            # procedure:
+            robustness_weights[np.isnan(robustness_weights)] = 0
+            W = np.dot(W, np.diag(robustness_weights))
+
         try: 
             # Equation 6.8 in FHT:
             BtWB = np.dot(np.dot(B.T, W), B)
@@ -137,6 +218,8 @@ def lowess(x, w, x0, kernel=epanechnikov, l=1):
         # the regression should not work for you and you should get a nan:
         except la.LinAlgError:
             ans[idx] += np.nan
+
+            
 
     return ans.T
 
